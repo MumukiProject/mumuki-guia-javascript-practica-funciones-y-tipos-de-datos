67cc645ca05eb7e1ab626183aad2347630495481
franco@mumuki.org <franco@mumuki.org>
2016-09-13 18:53:10 -0400

    Mumuki Export on 2016-09-13 18:53:10 -0400

--- 00003_Triangulos/test.js
+++ 00003_Triangulos/test.js
@@ -1,4 +1,7 @@
-describe("un triangulo", () => {
-  
-  
-})
+it("perimetroTriangulo(10, 10, 10) es 30", () => { assert.equal(perimetroTriangulo(10, 10, 10), 30) });
+it("perimetroTriangulo(12, 10, 10) es 32", () => { assert.equal(perimetroTriangulo(12, 10, 10), 32) });
+it("perimetroTriangulo(10, 14, 10) es 34", () => { assert.equal(perimetroTriangulo(10, 14, 10), 34) });
+it("perimetroTriangulo(10, 10, 15) es 35", () => { assert.equal(perimetroTriangulo(10, 10, 15), 35) });
+
+it("areaTriangulo(10, 10) es 50", () => { assert.equal(areaTriangulo(10, 10), 50) });
+it("areaTriangulo(10, 2) es 10", () => { assert.equal(areaTriangulo(10, 2), 10) });
