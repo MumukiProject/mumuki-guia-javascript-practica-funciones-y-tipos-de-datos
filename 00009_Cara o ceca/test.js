82084e90f2fd08933e26a083a9692975085e84fe
franco@mumuki.org <franco@mumuki.org>
2016-09-13 18:21:08 -0400

    Mumuki Export on 2016-09-13 18:21:08 -0400

--- 00009_Cara o ceca/test.js
+++ 00009_Cara o ceca/test.js
@@ -1,1 +1,14 @@
-pending
+describe("decisionConMoneda", () => {
+  it("decisionConMoneda(\"cara\", \"pizzas\", \"empanadas\") es \"pizzas\"", () => {
+    assert.equal(decisionConMoneda("cara", "pizzas", "empanadas"), "pizzas")
+  })
+  it("decisionConMoneda(\"cara\", \"asado\", \"empanadas\") es \"asado\"", () => {
+    assert.equal(decisionConMoneda("cara", "asado", "empanadas"), "asado")
+  })
+  it("decisionConMoneda(\"ceca\", \"pizzas\", \"empanadas\") es \"empanadas\"", () => {
+    assert.equal(decisionConMoneda("ceca", "pizzas", "empanadas"), "empanadas")
+  })
+  it("decisionConMoneda(\"ceca\", \"pizzas\", \"pastas\") es \"pastas\"", () => {
+    assert.equal(decisionConMoneda("ceca", "pizzas", "pastas"), "pastas")
+  })
+})
