a6191be28fdc90225402bb88fd0a13b275724981
franco@mumuki.org <franco@mumuki.org>
2016-09-13 19:14:00 -0400

    Mumuki Export on 2016-09-13 19:14:00 -0400

--- 00013_¿Está cerca?/test.js
+++ 00013_¿Está cerca?/test.js
@@ -1,12 +1,10 @@
-describe("estaCerca", () => {
-  it("estaCerca(443)", () => { assert(estaCerca(443)) })
-  it("estaCerca(442)", () => { assert(estaCerca(442)) })
-  it("estaCerca(441)", () => { assert(estaCerca(441)) })
-  it("estaCerca(439)", () => { assert(estaCerca(439)) })
-  it("estaCerca(438)", () => { assert(estaCerca(438)) })
-  it("estaCerca(437)", () => { assert(estaCerca(437)) })
-  
-  it("NO estaCerca(440)", () => { assert(!estaCerca(440)) })
-  it("NO estaCerca(420)", () => { assert(!estaCerca(420)) })
-  it("NO estaCerca(460)", () => { assert(!estaCerca(460)) })
-})
+it("estaCerca(443)", () => { assert(estaCerca(443)) })
+it("estaCerca(442)", () => { assert(estaCerca(442)) })
+it("estaCerca(441)", () => { assert(estaCerca(441)) })
+it("estaCerca(439)", () => { assert(estaCerca(439)) })
+it("estaCerca(438)", () => { assert(estaCerca(438)) })
+it("estaCerca(437)", () => { assert(estaCerca(437)) })
+
+it("NO estaCerca(440)", () => { assert(!estaCerca(440)) })
+it("NO estaCerca(420)", () => { assert(!estaCerca(420)) })
+it("NO estaCerca(460)", () => { assert(!estaCerca(460)) })
