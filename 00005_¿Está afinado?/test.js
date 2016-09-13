fd8f6fc48872e320975688ff44668acc51e2d6a4
franco@mumuki.org <franco@mumuki.org>
2016-09-13 18:18:09 -0400

    Mumuki Export on 2016-09-13 18:18:09 -0400

--- 00005_¿Está afinado?/test.js
+++ 00005_¿Está afinado?/test.js
@@ -1,1 +1,7 @@
-describe("", () => {})
+describe("estaAfinado", () => {
+  it("NO estaAfinado(443)", () => { assert(!estaAfinado(443)) })
+  it("NO estaAfinado(442)", () => { assert(!estaAfinado(442)) })
+  it("NO estaAfinado(437)", () => { assert(!estaAfinado(437)) })
+  
+  it("estaAfinado(440)", () => { assert(estaAfinado(440)) })
+})
