7875076e3ef27300232fe580bf8eec102e04c457
franco@mumuki.org <franco@mumuki.org>
2016-09-13 19:16:27 -0400

    Mumuki Export on 2016-09-13 19:16:27 -0400

--- 00006_Cartelitos/test.js
+++ 00006_Cartelitos/test.js
@@ -1,8 +1,6 @@
-desribe("escribirCartelito", () => {
-  it("escribirCartelito(\"Dra.\", \"Ana\", \"Pérez\"), es \"Dra. Ana Pérez\"", () => {
-    assert.equal(escribirCartelito("Dra.", "Ana", "Pérez"), "Dra. Ana Pérez")
-  })
-  it("escribirCartelito(\"Dr.\", \"Julio\", \"Gelman\"), es \"Dr. Julio Gelman\"", () => {
-    assert.equal(escribirCartelito("Dr.", "Julio", "Gelman"), "Dr. Julio Gelman")
-  })
+it("escribirCartelito(\"Dra.\", \"Ana\", \"Pérez\"), es \"Dra. Ana Pérez\"", () => {
+  assert.equal(escribirCartelito("Dra.", "Ana", "Pérez"), "Dra. Ana Pérez")
+})
+it("escribirCartelito(\"Dr.\", \"Julio\", \"Gelman\"), es \"Dr. Julio Gelman\"", () => {
+  assert.equal(escribirCartelito("Dr.", "Julio", "Gelman"), "Dr. Julio Gelman")
 })
