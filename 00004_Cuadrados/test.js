275a29fd5c1b560874e3ed6ef8787383099e4959
franco@mumuki.org <franco@mumuki.org>
2016-09-13 18:56:08 -0400

    Mumuki Export on 2016-09-13 18:56:08 -0400

--- 00004_Cuadrados/test.js
+++ 00004_Cuadrados/test.js
@@ -1,1 +1,5 @@
-describe("cuadrado", () => {})
+it("perimetroCuadrado(10) es 40", () => { assert.equal(perimetroCuadrado(10), 40) });
+it("perimetroCuadrado(20) es 80", () => { assert.equal(perimetroCuadrado(20), 80) });
+
+it("areaCuadrado(10) es 100", () => { assert.equal(areaCuadrado(10), 100) });
+it("areaCuadrado(5) es 25", () => { assert.equal(areaCuadrado(5), 25) });
