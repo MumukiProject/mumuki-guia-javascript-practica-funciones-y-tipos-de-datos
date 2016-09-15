c24899f51c3c01376235f1ece4e110d95a81d14a
franco@mumuki.org <franco@mumuki.org>
2016-09-14 22:18:22 -0400

    Mumuki Export on 2016-09-14 22:18:22 -0400

---
+++ extra.js
@@ -0,0 +1,31 @@
+function longitud(secuencia) {
+  return secuencia.length; 
+}
+
+function rango(s, e) {
+  let r = [];
+  for (let i = s; i <= e; i++) {
+     r.push(i)
+  }
+  return r;
+}
+
+function imprimir(s) {
+  console.log(s)
+}
+
+function poner(a, e) {
+   array.push(e);
+}
+
+function absoluto(x) {
+  return Math.abs(x)
+}
+
+function convertirEnMayuscula(x) {
+  return x.toUpperCase()
+}
+
+function comienzaCon(x, y) {
+  return x.startsWith(x, y);
+}
