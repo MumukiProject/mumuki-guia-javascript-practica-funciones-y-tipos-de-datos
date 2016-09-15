7e2f658ceedf9a37ab014282d776c720e4c28339
franco@mumuki.org <franco@mumuki.org>
2016-09-14 22:25:57 -0400

    Mumuki Export on 2016-09-14 22:25:57 -0400

--- 00008_Cartelitos óptimos/test.js
+++ 00008_Cartelitos óptimos/test.js
@@ -1,4 +1,8 @@
-it("escribe un cartelito largo cuando el nombre completo es corto", () => { assert.equal(escribirCartelitoOptimo("Ing.", "Carla", "Toledo"), "Ing. Carla Toledo")  })
-it("escribe un cartelito corto cuando el nombre completo es largo", () => { assert.equal(escribirCartelitoOptimo("Dr.", "Estanislao", "Schwarzschild"), "Ing. Schwarzschild")  })
+it("escribe un cartelito largo cuando el nombre completo es corto", () => { 
+  assert.equal(escribirCartelitoOptimo("Ing.", "Carla", "Toledo"), "Ing. Carla Toledo")
+})
+it("escribe un cartelito corto cuando el nombre completo es largo", () => { 
+  assert.equal(escribirCartelitoOptimo("Dr.", "Estanislao", "Schwarzschild"), "Ing. Schwarzschild")
+})
   
   
