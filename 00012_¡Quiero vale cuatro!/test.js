8751cc7bd6c6f7dbee9da67e33645b75857658ea
franco@mumuki.org <franco@mumuki.org>
2016-09-14 22:35:50 -0400

    Mumuki Export on 2016-09-14 22:35:50 -0400

--- 00012_¡Quiero vale cuatro!/test.js
+++ 00012_¡Quiero vale cuatro!/test.js
@@ -1,1 +1,11 @@
-pending
+it("valorCantoTruco(\"retruco\") es 3", ()  => {
+  assert.equal(valorCantoTruco("retruco"), 3)
+})
+
+it("valorCantoTruco(\"truco\") es 2", ()  => {
+  assert.equal(valorCantoTruco("truco"), 2)
+})
+
+it("valorCantoTruco(\"vale cuatro\") es 4", ()  => {
+  assert.equal(valorCantoTruco("vale cuatro"), 4)
+})
