5212a60bdcb07ad07e8e99ff1526fb47a62c2221
Federico Aloi <federico.aloi@gmail.com>
2016-10-21 15:32:03 -0300

    Update test.js

--- 00008_Cartelitos óptimos/test.js
+++ 00008_Cartelitos óptimos/test.js
@@ -2,7 +2,7 @@ it("escribe un cartelito largo cuando el nombre completo es corto", () => {
   assert.equal(escribirCartelitoOptimo("Ing.", "Carla", "Toledo"), "Ing. Carla Toledo")
 })
 it("escribe un cartelito corto cuando el nombre completo es largo", () => { 
-  assert.equal(escribirCartelitoOptimo("Dr.", "Estanislao", "Schwarzschild"), "Ing. Schwarzschild")
+  assert.equal(escribirCartelitoOptimo("Dr.", "Estanislao", "Schwarzschild"), "Dr. Schwarzschild")
 })
   
   
