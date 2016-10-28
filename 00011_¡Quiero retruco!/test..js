12f401cc1a704bfb02b170e243eb4394e4b89038
federico.aloi@gmail.com <federico.aloi@gmail.com>
2016-10-28 11:50:18 -0400

    Mumuki Export on 2016-10-28 11:50:18 -0400

--- 00011_¡Quiero retruco!/test..js
+++ 00011_¡Quiero retruco!/test..js
@@ -2,6 +2,10 @@ it("puntosDeEnvidoTotales(4, \"espadas\", 3, \"espadas\") es 27", ()  => {
   assert.equal(puntosDeEnvidoTotales(4, "espadas", 3, "espadas"), 27)
 })
 
+it("puntosDeEnvidoTotales(6, \"copas\", 11, \"copas\") es 26", ()  => {
+  assert.equal(puntosDeEnvidoTotales(6, "copas", 11, "copas"), 26)
+})
+
 it("puntosDeEnvidoTotales(8, \"oro\", 2, \"bastos\") es 8", ()  => {
   assert.equal(puntosDeEnvidoTotales(8, "oro", 2, "bastos"), 8)
 })
