ab763e0c0a0637955ae639158833b6d4704bba11
franco@mumuki.org <franco@mumuki.org>
2020-03-30 15:57:02 +0000

    Mumuki Export on 2020-03-30 15:57:02 +0000

--- 00010_¡Envido!/test..js
+++ 00010_¡Envido!/test..js
@@ -5,3 +5,16 @@ it("valorEnvido(5) es 5", () => { assert.equal(valorEnvido(5), 5) })
 it("valorEnvido(12) es 0", () => { assert.equal(valorEnvido(12), 0) })
 it("valorEnvido(11) es 0", () => { assert.equal(valorEnvido(11), 0) })
 it("valorEnvido(10) es 0", () => { assert.equal(valorEnvido(10), 0) })
+
+
+describe("valorEnvido", () => {
+  
+  try {
+    let resultad o = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12].every(it => typeof(valorEnvido(it)) !== 'string');
+    it("no devuelve strings", () => {
+      assert(resultado)
+    })
+  } catch (e) {}
+  
+  
+})
