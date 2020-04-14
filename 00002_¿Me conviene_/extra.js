83049a13f2d5ff45087f23df3aa331e36354b6cb
franco@mumuki.org <franco@mumuki.org>
2020-04-14 21:39:04 +0000

    Mumuki Export on 2020-04-14 21:39:04 +0000

--- 00002_¿Me conviene_/extra.js
+++ 00002_¿Me conviene_/extra.js
@@ -1,3 +1,10 @@
 function cuantoCuesta(pulgadas, memoria) {
+  if (typeof(pulgadas) !== 'number') {
+    throw "el primer argumento de cuantoCuesta debe ser un número"
+  }
+  if (typeof(memoria) !== 'number') {
+    throw "el segundo argumento de cuantoCuesta debe ser un número"
+  }
+  
   return 60 * pulgadas + 200 * memoria + 1000
 }
