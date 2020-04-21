286af2e49098532f6fd3121f9e7a19a60d5e2190
franco@mumuki.org <franco@mumuki.org>
2020-04-21 18:07:02 +0000

    Mumuki Export on 2020-04-21 18:07:02 +0000

--- extra.js
+++ extra.js
@@ -1,31 +1,37 @@
-function longitud(secuencia) {
-  return secuencia.length; 
-}
+/*<hidden-for-student@*/
+// Ver https://github.com/MumukiProject/mumuki-apendice-imperativa-javascript/blob/master/biblioteca.js
 
-function rango(s, e) {
-  let r = [];
-  for (let i = s; i <= e; i++) {
-     r.push(i)
-  }
-  return r;
-}
+// =============
+// Lección 1 y 2
+// =============
+/*@hidden-for-student>*/
 
-function imprimir(s) {
-  console.log(s)
-}
+// Nos dice cuan largo es un string
+//
+// Por ejemplo:
+//
+//  ム longitud("hola")
+//  4
+function longitud(unString) /*<elipsis-for-student@*/ {
+  return unString.length;
+} /*@elipsis-for-student>*/
 
-function poner(a, e) {
-   array.push(e);
-}
+// Convierte una palabra en mayúsculas
+//
+// Por ejemplo:
+//
+//  ム convertirEnMayuscula("hola")
+//  "HOLA"
+function convertirEnMayuscula(unString) /*<elipsis-for-student@*/ {
+  return unString.toUpperCase()
+} /*@elipsis-for-student>*/
 
-function absoluto(x) {
-  return Math.abs(x)
-}
-
-function convertirEnMayuscula(x) {
-  return x.toUpperCase()
-}
-
-function comienzaCon(x, y) {
-  return x.startsWith(x, y);
-}
+// Nos dice si unString empieza con otroString
+//
+// Por ejemplo:
+//
+//  ム comienzaCon("hola todo el mundo", "hola todo")
+//  true
+function comienzaCon(unString, otroString) /*<elipsis-for-student@*/ {
+  return unString.startsWith(otroString);
+} /*@elipsis-for-student>*/
