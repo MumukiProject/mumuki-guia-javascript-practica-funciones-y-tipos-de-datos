ad8b3b02fef7f6a46a91d659f999c6b05107a7d5
gustavo@mumuki.org <gustavo@mumuki.org>
2020-08-03 20:57:06 +0000

    Mumuki Export on 2020-08-03 20:57:06 +0000

--- 00008_Cartelitos óptimos/extra.js
+++ 00008_Cartelitos óptimos/extra.js
@@ -1,3 +1,4 @@
+
 // Nos retorná el texto de un cartelito. El último parámetro determina si va a ser corto o largo  cuan largo es un string
 //
 // Por ejemplo:
@@ -7,8 +8,8 @@
 //  ム escribirCartelito("Dra.", "Zelika", "Vera", false)
 //  "Dra. Zelika Vera"
 
-function escribirCartelito(titulo, nombre, apellido, quiere_corto) /*<elipsis-for-student@*/ {
-  if(quiere_corto) {
+function escribirCartelito(titulo, nombre, apellido, quiere_cartel_corto) /*<elipsis-for-student@*/ {
+  if(quiere_cartel_corto) {
     return titulo + " " + apellido;
   }
   else {
