59fc5ff3a255a0bbad71acf962fa1d4bf82eb835
gustavo@mumuki.org <gustavo@mumuki.org>
2020-08-04 21:29:14 +0000

    Mumuki Export on 2020-08-04 21:29:14 +0000

--- extra.js
+++ extra.js
@@ -6,32 +6,36 @@
 // =============
 /*@hidden-for-student>*/
 
-// Nos dice cuan largo es un string
+function longitud(unString) /*<elipsis-for-student@*/ {
+  return unString.length;
+} /*@elipsis-for-student>*/
+// Retorna cuan largo es un string
 //
 // Por ejemplo:
 //
 //  ム longitud("hola")
 //  4
-function longitud(unString) /*<elipsis-for-student@*/ {
-  return unString.length;
-} /*@elipsis-for-student>*/
 
+//==============================================================
+
+function convertirEnMayuscula(unString) /*<elipsis-for-student@*/ {
+  return unString.toUpperCase()
+} /*@elipsis-for-student>*/
 // Convierte una palabra en mayúsculas
 //
 // Por ejemplo:
 //
 //  ム convertirEnMayuscula("hola")
 //  "HOLA"
-function convertirEnMayuscula(unString) /*<elipsis-for-student@*/ {
-  return unString.toUpperCase()
-} /*@elipsis-for-student>*/
 
-// Nos dice si unString empieza con otroString
+//==============================================================
+
+function comienzaCon(unString, otroString) /*<elipsis-for-student@*/ {
+  return unString.startsWith(otroString);
+} /*@elipsis-for-student>*/
+// Retorna un booleano que nos dice si unString empieza con otroString
 //
 // Por ejemplo:
 //
 //  ム comienzaCon("hola todo el mundo", "hola todo")
 //  true
-function comienzaCon(unString, otroString) /*<elipsis-for-student@*/ {
-  return unString.startsWith(otroString);
-} /*@elipsis-for-student>*/
