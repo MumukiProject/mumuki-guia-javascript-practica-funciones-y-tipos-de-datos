63359aa07530d4664b5f1da3978bc7bf475b5766
rocio@mumuki.org <rocio@mumuki.org>
2021-09-16 15:51:24 +0000

    Mumuki Export on 2021-09-16 15:51:24 +0000

--- 00011_¡Quiero retruco!/test..js
+++ 00011_¡Quiero retruco!/test..js
@@ -9,3 +9,7 @@ it("puntosDeEnvidoTotales(6, \"copas\", 11, \"copas\") es 26", ()  => {
 it("puntosDeEnvidoTotales(6, \"copas\", 7, \"espadas\") es 7", ()  => {
   assert.equal(puntosDeEnvidoTotales(6, "copas", 7, "espadas"), 7)
 })
+
+it("puntosDeEnvidoTotales(1, \"espadas\", 12, \"bastos\") es 1", ()  => {
+  assert.equal(puntosDeEnvidoTotales(1, "espadas", 12, "bastos"), 1)
+})
